fd047f6019543e2260a04c3a206347b7e3a7339c
Phillip Chan <phillipchan@Phillips-MacBook-Pro.local>
2023-10-14 14:29:35 -0700

    Add firebase boilerplate

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCgI1vXRv4bpPp7su0Zk4ih6jHoPBjrALc",
+  authDomain: "promptflow-3957d.firebaseapp.com",
+  projectId: "promptflow-3957d",
+  storageBucket: "promptflow-3957d.appspot.com",
+  messagingSenderId: "384163400756",
+  appId: "1:384163400756:web:e2827be9c13ed13f754e61",
+  measurementId: "G-FWZ4PK08XV",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
